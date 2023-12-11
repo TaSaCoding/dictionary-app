d5930e975a652932fb6b5ab5f20360fe919506e1
TaSaCoding <142542869+TaSaCoding@users.noreply.github.com>
2023-12-11 19:54:27 +0100

    search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
-import './App.css';
+import "./App.css";
+import logo from "./logo.png";
+import Dictionary from "./Dictionary";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">hello
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo"/>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          Coded by Tanja Sauer, open soureced on{" "}
+          <a
+            href="https://github.com/TaSaCoding/dictionary-app.git"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+          , hosted on{" "}
+          <a
+            href="https://tsd-dictionary-app.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
