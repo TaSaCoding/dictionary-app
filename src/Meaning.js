b2e5b454c571bbf40120f7424adbf7f8249bff84
TaSaCoding <142542869+TaSaCoding@users.noreply.github.com>
2023-12-15 19:27:08 +0100

    getting the synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,12 +1,23 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
-  console.log(props.meaning);
-  return (
+  if (props.meaning){
+    return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      <p>{props.meaning.definition}</p>
-      <em>{props.meaning.example}</em>
+      <p>
+        <strong>Definition: </strong>
+        {props.meaning.definition}
+        <br />
+        <strong>Example: </strong>
+        <em>{props.meaning.example}</em>
+        <br />
+        <Synonyms synonyms={props.meaning.synonyms}/>
+      </p>
     </div>
   );
+  }else {
+    return null;
+  }
 }
