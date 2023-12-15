da2445eccda6d3cd54b15aeed695105266ee8777
TaSaCoding <142542869+TaSaCoding@users.noreply.github.com>
2023-12-16 00:36:33 +0100

    Photo integration/ API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,16 +1,20 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
-    console.log(response.data);
+  function handleDictionaryResponse(response) {
     setResults(response.data);
+  }
+  function handleimageResponse(response) {
+    setPhotos(response.data.photos);
   }
   function load() {
     setLoaded(true);
@@ -19,7 +23,11 @@ export default function Dictionary(props) {
   function search() {
     let apiKey = "bf33d2o4a3a21af9f8ac140tee13c0ea";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let imageApiKey = "bf33d2o4a3a21af9f8ac140tee13c0ea";
+    let imageApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${imageApiKey}`;
+    axios.get(imageApiUrl).then(handleimageResponse);
   }
   function handleSubmit(event) {
     event.preventDefault();
@@ -47,6 +55,7 @@ export default function Dictionary(props) {
           </div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
